6965945cba6413ddb48896f9d3a4b538b6af1c44
Sean Eric Thomas <set@seanericthomas.com>
2022-12-26 18:18:14 -0800

    Make number buttons fill display

--- script.js
+++ script.js
@@ -28,3 +28,39 @@ function operate(operator, a, b) {
       return 1;
   }
 }
+
+const state = {
+  displayedValue: '0',
+  pendingOperation: {
+    operandA: 0,
+    operandB: 0,
+    operator: '',
+  },
+  updateDisplay: function () {
+    const display = document.querySelector('.display');
+    display.textContent = state.displayedValue;
+  },
+};
+
+function buttonListeners() {
+  const buttons = document.querySelectorAll('.button');
+  for (let i = 0; i < buttons.length; i += 1) {
+    // Numbers will update the display accordingly
+    if (buttons[i].classList.contains('number')) {
+      buttons[i].addEventListener('click', () => {
+        // Limit by about how many numbers can fit on the display
+        if (state.displayedValue.length < 13) {
+          // Get rid of the leading 0 if there's no decimal
+          if (state.displayedValue[0] === '0' &&
+              !state.displayedValue.includes('.')) {
+            state.displayedValue = '';
+          }
+          state.displayedValue += buttons[i].textContent;
+          state.updateDisplay();
+        }
+      });
+    }
+  }
+}
+
+buttonListeners();
